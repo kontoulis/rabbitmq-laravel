d1b6261dd51576d0222adfca1ed7dc3f6b708a9d
Bart Verwilst <bart@verwilst.be>
2017-04-28 15:18:11 +0200

    add exchange var, and dotenv-ify it

--- src/config.php
+++ src/config.php
@@ -5,12 +5,14 @@
  * Date: 12/9/2015
  * Time: 1:24 μμ
  */
+
 return [
 // AMPQ Connection settigns for RabbitMQ
-	'amqp_host' => 'localhost',
-	'amqp_port' => 5672,
-	'amqp_user' => 'guest',
-	'amqp_pass' => 'guest',
-	'amqp_vhost' => '/',
-	'amqp_queue' => 'MyQueue'
+    'amqp_host' => env('RABBITMQ_HOST', '127.0.0.1'),
+    'amqp_port' => 5672,
+    'amqp_user' => env('RABBITMQ_USER', 'guest'),
+    'amqp_pass' => env('RABBITMQ_PASS', 'guest'),
+    'amqp_vhost' => env('RABBITMQ_VHOST', '/'),
+    'amqp_queue' => env('RABBITMQ_QUEUE', 'queue'),
+    'amqp_exchange' => env('RABBITMQ_EXCHANGE', '')
 ];
