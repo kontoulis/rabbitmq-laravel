51246baa63bc4d7eb9b2c57675ca0ad437d9de30
Asmodai <kontoulisdhm@gmail.com>
2018-01-03 11:50:28 +0200

    local changes

--- src/Broker/Broker.php
+++ src/Broker/Broker.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: verwilst
- * Date: 2/05/17
- * Time: 21:49
- */
 
 namespace Kontoulis\RabbitMQLaravel\Broker;
 
@@ -14,122 +8,202 @@ use Kontoulis\RabbitMQLaravel\Exception\BrokerException;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Exception\AMQPRuntimeException;
 use PhpAmqpLib\Channel\AMQPChannel;
+use PhpAmqpLib\Exception\AMQPTimeoutException;
 use PhpAmqpLib\Message\AMQPMessage;
 
+/**
+ * Class Broker
+ * @package Kontoulis\RabbitMQLaravel\Broker
+ */
 class Broker extends AMQPChannel
 {
-    private $queueName;
-    private $host;
-    private $port;
-    private $user;
-    private $password;
-    private $vhost;
+    /**
+     * @var string
+     */
+    protected $exchange = '';
+    /**
+     * @var
+     */
+    protected $host;
+    /**
+     * @var
+     */
+    protected $port;
+    /**
+     * @var
+     */
+    protected $user;
+    /**
+     * @var
+     */
+    protected $password;
+    /**
+     * @var
+     */
+    protected $vhost;
+    /**
+     * @var
+     */
+    protected $defaultQueue;
 
     /**
-     * @param $config
-     * @throws Kontoulis\RabbitMQLaravel\Exception\BrokerException
+     * If set, the consumer will exit after the timeout is passed (e.g. if the queue is empty after the timeout passes)
+     * @var
+     */
+    protected $consumeTimeout = 0 ;
+
+    /**
+     * Broker constructor.
+     * @param \PhpAmqpLib\Connection\AbstractConnection $config
      */
     function __construct($config)
     {
-
         $this->host = $config['amqp_host'];
         $this->port = $config['amqp_port'];
         $this->user = $config['amqp_user'];
         $this->password = $config['amqp_pass'];
         $this->vhost = $config['amqp_vhost'];
+        $this->defaultQueue = (!empty($config["amqp_default_queue"]) ? $config["amqp_default_queue"] : env("APP_NAME")."_queue");
 
         try {
-
             /* Open RabbitMQ connection */
-
             $connection = new AMQPStreamConnection($this->host, $this->port, $this->user, $this->password, $this->vhost);
             parent::__construct($connection);
 
         } catch (AMQPRuntimeException $ex) {
-
             throw new BrokerException(
-
                 'Fatal error while initializing AMQP connection: '
-
                 . $ex->getMessage(),
-
                 $ex->getCode()
-
             );
-
         }
     }
-    public function listenToQueue($handlers = [] , $queueName = null )
-    {
-        if(!is_null($queueName)) {
-            $this->queueName = $queueName;
-        }
-        /* Look for handlers */
 
+    /**
+     * @param int $timeout
+     */
+    public function setConsumeTimeout($timeout = 0){
+        $this->consumeTimeout = $timeout;
+    }
+
+    /**
+     * @param $exchange
+     * @param string $type
+     */
+    public function setExchange($exchange, $type = "direct")
+    {
+        $this->exchange = $exchange;
+        $this->exchange_declare($exchange, $type, false, true, false);
+    }
+
+    /**
+     * @param $message
+     * @param null $routingKey
+     */
+    public function publishMessage($message, $routingKey = null)
+    {
+        $this->queueDeclareBind($routingKey);
+        $msg = new Message($message, $routingKey);
+        // Create the message
+        $amqpMessage = $msg->getAMQPMessage();
+        $this->basic_publish($amqpMessage, $this->exchange, $routingKey);
+    }
+
+    /**
+     * @param $messages
+     * @param null $routingKey
+     */
+    public function publishBatch($messages, $routingKey = null)
+    {
+        $this->queueDeclareBind($routingKey);
+        // Create the messages
+        foreach ($messages as $message) {
+            $this->batch_basic_publish(
+                (new Message($message))->getAMQPMessage(), $this->exchange, $routingKey
+            );
+        }
+        $this->publish_batch();
+    }
+
+    /**
+     * @param callable $callback
+     * @param null $routingKey
+     * @param array $options
+     * @return bool
+     */
+    public function basicConsume(callable $callback, $routingKey = null, $options = [])
+    {
+        $this->queueDeclareBind($routingKey);
+
+        /* Start consuming */
+        $this->basic_qos(
+            (isset($options["prefetch_size"]) ? $options["prefetch_size"] : null),
+            (isset($options["prefetch_count"]) ? $options["prefetch_count"] : 1),
+            (isset($options["a_global"]) ? $options["a_global"] : null)
+        );
+
+        $this->basic_consume(
+            $routingKey,
+            (isset($options["consumer_tag"]) ? $options["consumer_tag"] : ''),
+            (isset($options["no_local"]) ? (bool)$options["no_local"] : false),
+            (isset($options["no_ack"]) ? (bool)$options["no_ack"] : false),
+            (isset($options["exclusive"]) ? (bool)$options["exclusive"] : false),
+            (isset($options["no_wait"]) ? (bool)$options["no_wait"] : false),
+            function (AMQPMessage $amqpMsg) use($callback) {
+                $msg = Message::fromAMQPMessage($amqpMsg);
+                return  call_user_func($callback, $msg);;
+            }
+        );
+
+        return $this->waitConsume($options);
+    }
+
+    /**
+     * Starts to listen to a queue for incoming messages.
+     * @param array $handlers Array of handler class instances
+     * @param null $routingKey
+     * @param array $options
+     * @return bool
+     * @internal param string $queueName The AMQP queue
+     */
+    public function listenToQueue(array $handlers , $routingKey = null, $options =[] )
+    {
+        /* Look for handlers */
         $handlersMap = array();
         foreach ($handlers as $handlerClassPath) {
-
             if (!class_exists($handlerClassPath)) {
-
                 $handlerClassPath = "Kontoulis\\RabbitMQLaravel\\Handlers\\DefaultHandler";
-
                 if (!class_exists($handlerClassPath)) {
-
                     throw new BrokerException(
                         "Class $handlerClassPath was not found!"
                     );
-
                 }
-
             }
-
             $handlerOb = new $handlerClassPath();
-
             $classPathParts = explode("\\", $handlerClassPath);
-
-            $handlersMap[$classPathParts[count(
-
-                $classPathParts
-
-            ) - 1]] = $handlerOb;
-
+            $handlersMap[$classPathParts[count($classPathParts) - 1]] = $handlerOb;
         }
 
-
-        /* Create queue */
-
-        $this->queue_declare(
-
-            $this->queueName, false, true, false, false
-
-        );
-
-
+        $this->queueDeclareBind($routingKey);
         /* Start consuming */
-
-        $this->basic_qos(null, 1, null);
-
-        $this->basic_consume(
-
-            $this->queueName, '', false, false, false, false, function (AMQPMessage $amqpMsg) use ($handlersMap) {
-
-//            dd($amqpMsg->get("delivery_tag"));
-            $msg = Message::fromAMQPMessage($amqpMsg);
-
-            $this->handleMessage($msg, $handlersMap);
-
-        }
-
+        $this->basic_qos(
+            (isset($options["prefetch_size"]) ? $options["prefetch_size"] : null),
+            (isset($options["prefetch_count"]) ? $options["prefetch_count"] : 1),
+            (isset($options["a_global"]) ? $options["a_global"] : null)
         );
-
-        /* Iterate until ctrl+c is received... */
-
-        while (count($this->callbacks)) {
-
-            $this->wait();
-
-        }
-
+        $this->basic_consume(
+            $routingKey,
+            (isset($options["consumer_tag"]) ? $options["consumer_tag"] : ''),
+            (isset($options["no_local"]) ? (bool)$options["no_local"] : false),
+            (isset($options["no_ack"]) ? (bool)$options["no_ack"] : false),
+            (isset($options["exclusive"]) ? (bool)$options["exclusive"] : false),
+            (isset($options["no_wait"]) ? (bool)$options["no_wait"] : false),
+            function (AMQPMessage $amqpMsg) use($handlersMap) {
+                $msg = Message::fromAMQPMessage($amqpMsg);
+                $this->handleMessage($msg, $handlersMap);
+            }
+        );
+        return $this->waitConsume($options);
     }
 
     /**
@@ -137,276 +211,128 @@ class Broker extends AMQPChannel
      * @param array   $handlersMap
      * @return bool
      */
-    public function handleMessage($msg, array $handlersMap)
+    public function handleMessage(Message $msg, array $handlersMap)
     {
-
         /* Try to process the message */
-
-        foreach ($handlersMap as $code => $ob) {
-
-            $retVal = $ob->tryProcessing($msg);
-
-//            $msg->updateAMQPMessage();
-
+        foreach ($handlersMap as $handler) {
+            $retVal = $handler->process($msg);
             switch ($retVal) {
-
                 case Handler::RV_SUCCEED_STOP:
-
                     /* Handler succeeded, you MUST stop processing */
-
-                    return $this->handleSucceedStop($msg);
-
+                    return $handler->handleSucceedStop($msg);
 
                 case Handler::RV_SUCCEED_CONTINUE:
-
                     /* Handler succeeded, you SHOULD continue processing */
-
-                    $this->handleSucceedContinue($msg);
-
+                    $handler->handleSucceedContinue($msg);
                     continue;
 
-
                 case Handler::RV_PASS:
-
                     /**
                      * Just continue processing (I have no idea what
                      * happened in the handler)
                      */
-
                     continue;
 
-
                 case Handler::RV_FAILED_STOP:
-
                     /* Handler failed and MUST stop processing */
-
-
-                    return $this->handleFailedStop($msg);
-
+                    return $handler->handleFailedStop($msg);
 
                 case Handler::RV_FAILED_REQUEUE:
-
                     /**
                      * Handler failed and MUST stop processing but the message
                      * will be rescheduled
                      */
+                    return $handler->handleFailedRequeue($msg);
 
-                    return $this->handleFailedRequeue($msg);
-
+                case Handler::RV_FAILED_REQUEUE_STOP:
+                    /**
+                     * Handler failed and MUST stop processing but the message
+                     * will be rescheduled
+                     */
+                    return $handler->handleFailedRequeueStop($msg, true);
 
                 case Handler::RV_FAILED_CONTINUE:
-
                     /* Well, handler failed, but you may try another */
-
-                    $this->handleFailedContinue($msg);
-
+                    $handler->handleFailedContinue($msg);
                     continue;
 
-
                 default:
-
                     return false;
-
             }
 
         }
-
         /* If haven't return yet, send an ACK */
-
-        $this->basic_ack($msg->get("delivery_tag"));
-
+        $msg->sendAck();
     }
 
     /**
-     * @param null $msg
-     * @return int
+     * @param array $options
+     * @return bool
      */
-    public function getStatus($msg = null)
+    protected function waitConsume($options = []){
+        $consume = true;
+        while (count($this->callbacks) && $consume) {
+            try{
+                $this->wait((isset($options["allowed_methods"]) ?$options["allowed_methods"] : null),
+                    (isset($options["non_blocking"]) ?$options["non_blocking"] : false), $this->consumeTimeout);
+            }
+            catch (AMQPTimeoutException $e){
+                if($e->getMessage() === "The connection timed out after {$this->consumeTimeout} sec while awaiting incoming data") {
+                    $consume = false;
+                }else{
+                    throw($e);
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * @param $routingKey
+     */
+    protected function queueDeclareBind(&$routingKey)
     {
+        if (is_null($routingKey)) {
+            // Set the routing key if missing
+            $routingKey = $this->defaultQueue;
+        }
 
-        $request = [
+        // Create/declare queue
+        $this->queue_declare($routingKey, false, true, false, false);
 
-            "count"    => 10,
+        if ($this->exchange != "") {
+            // Bind the queue to the exchange
+            $this->queue_bind($routingKey, $this->exchange, $routingKey);
+        }
+    }
 
-            "requeue"  => true,
-
-            "encoding" => "auto"
-
-        ];
-
-        if (!is_null($msg) && strlen($msg->get("routing_key")) > 0) {
-
-            $queueName = $msg->get("routing_key");
-
-        } else {
-
-            $queueName = $this->queueName;
-
+    /**
+     * @param null $routingKey
+     * @return mixed
+     */
+    public function getQueueInfo($routingKey = null)
+    {
+        if (is_null($routingKey)) {
+            // Set the routing key if missing
+            $routingKey = $this->defaultQueue;
         }
 
         $ch = curl_init();
-
-        $url = "http://" . $this->host . ":" . $this->port . "/api/queues/%2F/" .
-
-            $queueName . '/get';
-
-
-        $fields = json_encode($request);
+        $vhost = ($this->vhost != "/" ? $this->vhost : "%2F");
+        $url = "http://" . $this->host . ":15672" . "/api/queues/$vhost/" .$routingKey;
 
         curl_setopt($ch, CURLOPT_URL, $url);
 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-        curl_setopt($ch, CURLOPT_POST, count($request));
-
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
-
         curl_setopt($ch, CURLOPT_USERPWD, $this->user . ":" . $this->password);
 
         curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
 
-
         $result = curl_exec($ch);
         curl_close($ch);
 
-
-        $data = json_decode($result);
-
-        $messages = $data;
-        if (!empty($messages)) {
-            if (is_array($messages)) {
-                return $messages[0]->message_count;
-            } else {
-                return $messages->message_count;
-            }
-        } else {
-            return -1;
-        }
-
+        return json_decode($result);
     }
 
-    /**
-     * @param $queueName
-     */
-    public function setQueue($queueName){
-        $this->queueName = $queueName;
-    }
-
-    /**
-     * @param Message $msg
-     * @return bool
-     */
-
-    protected function handleSucceedStop($msg)
-    {
-        $this->basic_ack(
-
-            $msg->get("delivery_tag")
-
-        );
-
-        $remaining = $this->getStatus($msg);
-
-        if ($remaining < 1) {
-
-            exit(0);
-
-        }
-
-        return true;
-
-    }
-
-    /**
-     * @param Message $msg
-     * @return bool
-     */
-    protected function handleSucceedContinue($msg)
-    {
-        return true;
-    }
-
-
-    /**
-     * @param Message $msg
-     * @throws Kontoulis\RabbitMQLaravel\Exception\BrokerException
-     */
-    protected function handleFailedStop($msg)
-    {
-        $msg->sendNack();
-
-        throw new BrokerException(
-
-            "Handler failed for message"
-
-            . " {$msg->getDeliveryTag()}."
-
-            . " Execution stops but message is not rescheduled."
-
-        );
-    }
-
-
-    /**
-     * @param Message $msg$
-     * @return bool
-     */
-    protected function handleFailedRequeue(Message $msg)
-    {
-        $msg->sendNack();
-
-        $msg->republish();
-
-        return true;
-
-    }
-
-
-    /**
-     * @param Message $msg
-     * @return bool
-     */
-    protected function handleFailedContinue(Message $msg)
-    {
-        return true;
-    }
-
-    /**
-     * @param $queueName
-     * @param $message
-     * @internal param Kontoulis\RabbitMQLaravel\Message\Message $msg
-     */
-
-    public function sendMessage($message, $queueName=null)
-    {
-
-        if(is_null($queueName)){
-            $queueName = $this->queueName;
-        }
-
-        $msg = new Message($message);
-        /* Create the message */
-
-        $amqpMessage = $msg->getAMQPMessage();
-
-
-        /* Create queue */
-
-        $this->queue_declare(
-
-            $queueName, false, true, false, false
-
-        );
-
-
-        /* Publish message */
-
-        $this->basic_publish(
-
-            $amqpMessage, '', $msg->queueName
-
-        );
-
-
-    }
 }
--- src/config.php
+++ src/config.php
@@ -13,6 +13,6 @@ return [
     'amqp_user' => env('RABBITMQ_USER', 'guest'),
     'amqp_pass' => env('RABBITMQ_PASS', 'guest'),
     'amqp_vhost' => env('RABBITMQ_VHOST', '/'),
-    'amqp_queue' => env('RABBITMQ_QUEUE', 'queue'),
+    'amqp_default_queue' => env('RABBITMQ_DEFAULT_QUEUE', 'queue'),
     'amqp_exchange' => env('RABBITMQ_EXCHANGE', '')
 ];
